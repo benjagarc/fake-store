57a482b819bdad11de2edcf5f41f3d53f9e05487
bgarciabtk <bgarcia@btkfarma.com>
2025-04-17 14:30:26 -0600

    create route to categories

---
+++ app/category/[category]/page.tsx
@@ -0,0 +1,45 @@
+import { notFound } from "next/navigation";
+import { Metadata } from "next";
+import { Product } from "@/components/molecules/Card/interface";
+import Row from "react-bootstrap/esm/Row";
+import Card from "@/components/molecules/Card";
+import { Params } from "./types";
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  return {
+    title: `${params.category} | Fake Store`,
+  };
+}
+
+export default async function CategoryPage({ params }: Params) {
+  const response = await fetch(
+    `https://fakestoreapi.com/products/category/${params.category}`,
+    {
+      next: { revalidate: 60 },
+    }
+  );
+
+  if (!response.ok) return notFound();
+
+  const products: Product[] = await response.json();
+
+  return (
+    <div>
+      <h1 className="mb-4 text-capitalize">{params.category}</h1>
+      <Row xs={2} sm={2} md={3} lg={4} className="g-4">
+        {products.map((product) => (
+          <Card key={product?.id} {...product} />
+        ))}
+      </Row>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const response = await fetch("https://fakestoreapi.com/products/categories");
+  const categories: string[] = await response.json();
+
+  return categories.map((category) => ({
+    category,
+  }));
+}
