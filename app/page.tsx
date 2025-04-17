c6d1144312479d39a5b4e0464fe907149ffaa498
bgarciabtk <bgarcia@btkfarma.com>
2025-04-17 11:43:39 -0600

    print cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-import Button from "react-bootstrap/Button";
+"use client";
+import { useEffect, useState } from "react";
+import { Product } from "@/components/molecules/Card/interface";
+import { getAllProducts } from "@/request/products";
+import Spinner from "react-bootstrap/esm/Spinner";
+import Row from "react-bootstrap/Row";
+import Card from "@/components/molecules/Card";
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const _getProducts = async () => {
+    const data = await getAllProducts();
+    setProducts(() => data);
+    setLoading((prev) => !prev);
+  };
+
+  useEffect(() => {
+    _getProducts();
+  }, []);
+
   return (
-    <div>
-      <Button>Click me!</Button>
-    </div>
+    <>
+      {loading && (
+        <div className="text-center">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )}
+      <Row xs={2} sm={2} md={3} lg={4} className="g-4">
+        {products.map((product) => (
+          <Card key={product?.id} {...product} />
+        ))}
+      </Row>
+    </>
   );
 }
