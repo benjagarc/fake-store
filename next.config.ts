a13be6bfce85360767b7cfc3afdaa140332f079b
bgarciabtk <bgarcia@btkfarma.com>
2025-04-17 14:29:30 -0600

    set railing slash

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
   env: {
     API_URL: process.env.API_URL,
   },
   images: {
-    remotePatterns: [new URL('https://fakestoreapi.com/img/**')],
+    remotePatterns: [new URL("https://fakestoreapi.com/img/**")],
   },
 };
 
