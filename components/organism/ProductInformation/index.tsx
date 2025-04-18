200da3d649fa886479c8ebbe1348c53120818304
bgarciabtk <bgarcia@btkfarma.com>
2025-04-17 21:36:45 -0600

    set animation to Product information

--- components/organism/ProductInformation/index.tsx
+++ components/organism/ProductInformation/index.tsx
@@ -7,26 +7,34 @@ import Col from "react-bootstrap/Col";
 import stylesCard from "@/components/molecules/Card/index.module.scss";
 import ProductDescription from "@/components/molecules/ProductDescription";
 import { FC, memo } from "react";
+import { motion } from "framer-motion";
 
 export const ProductInformation: FC<Product> = (product) => {
   return (
     <>
-      <Row className="g-4">
-        <Col md={6}>
-          <div className={`${stylesCard.containerImg} overflow-hidden`}>
-            <Image
-              src={product?.image ?? ""}
-              alt={product?.title ?? ""}
-              width={400}
-              height={400}
-              className="img-fluid"
-              priority
-            />
-          </div>
-        </Col>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className="h-100 shadow-sm py-5"
+      >
+        <Row className="g-4">
+          <Col md={6}>
+            <div className={`${stylesCard.containerImg} overflow-hidden`}>
+              <Image
+                src={product?.image ?? ""}
+                alt={product?.title ?? ""}
+                width={400}
+                height={400}
+                className="img-fluid"
+                priority
+              />
+            </div>
+          </Col>
 
-        <Col md={6}>{<ProductDescription {...product} />}</Col>
-      </Row>
+          <Col md={6}>{<ProductDescription {...product} />}</Col>
+        </Row>
+      </motion.div>
     </>
   );
 };
